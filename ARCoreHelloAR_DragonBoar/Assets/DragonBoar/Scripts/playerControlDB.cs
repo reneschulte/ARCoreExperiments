8e49e4ec470a69220bcf78ef3b01b7e2645735a6
Rene Schulte <contact@rene-schulte.info>
2017-08-31 15:08:28 +0200

    Added ArCore DragonBoar

---
+++ ARCoreHelloAR_DragonBoar/Assets/DragonBoar/Scripts/playerControlDB.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControlDB : MonoBehaviour 
+{
+	public Animator anim;
+	int scream;
+	int basicAttack;
+	int getHit;
+	int walk;
+	int die;
+	int run;
+
+
+	void Awake () 
+	{
+		anim = GetComponent<Animator>();
+		scream = Animator.StringToHash("Scream");
+		basicAttack = Animator.StringToHash("Basic Attack");
+		getHit = Animator.StringToHash("Get Hit");
+		walk = Animator.StringToHash("Walk");
+		die = Animator.StringToHash("Die");
+		run = Animator.StringToHash ("Run");
+	}
+
+
+	public void Scream ()
+	{
+		anim.SetTrigger(scream);
+	}
+
+	public void BasicAttack ()
+	{
+		anim.SetTrigger(basicAttack);
+	}
+
+	public void GetHit ()
+	{
+		anim.SetTrigger(getHit);
+	}
+
+	public void Walk ()
+	{
+		anim.SetTrigger(walk);
+	}
+
+	public void Die ()
+	{
+		anim.SetTrigger(die);
+	}
+
+	public void Run ()
+	{
+		anim.SetTrigger(run);
+	}
+		
+}
