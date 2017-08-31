d666d985045ead3f574e6af453fee3e09a4f3f17
Rene Schulte <contact@rene-schulte.info>
2017-08-31 14:57:50 +0200

    Added base physics demo objects

---
+++ ARCorePhysics/Assets/Scripts/CannonBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+
+public class CannonBehavior : MonoBehaviour
+{
+    public float ForceMagnitude = 300f;
+    public GameObject GazeCursor;
+    public Material CannonMaterial;
+    public AudioSource ShootSound;
+    public AudioClip CollisionClip;
+
+    public void Shoot()
+    {
+        //    ShootSound.Play();
+
+        var eyeball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        eyeball.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        eyeball.GetComponent<Renderer>().material = CannonMaterial;
+
+        var rigidBody = eyeball.AddComponent<Rigidbody>();
+        rigidBody.mass = 0.5f;
+        rigidBody.position = transform.position;
+        var forward = transform.forward;
+        forward = Quaternion.AngleAxis(-10, transform.right) * forward;
+        rigidBody.AddForce(forward * ForceMagnitude);
+
+        eyeball.AddComponent<AudioCollisionBehaviour>().SoundSoftCrash = CollisionClip;
+    }
+
+    void Update()
+    {
+        if (GazeCursor == null) return;
+
+        var raycastHits = Physics.RaycastAll(transform.position, transform.forward);
+        var firstHit = raycastHits.OrderBy(r => r.distance).FirstOrDefault();
+
+        GazeCursor.transform.position = firstHit.point;
+        GazeCursor.transform.forward = firstHit.normal;
+    }
+}
