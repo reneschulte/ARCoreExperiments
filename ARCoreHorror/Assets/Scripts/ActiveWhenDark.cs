31e52e23bc078dcfaed8a6f8e871235fc737f3f9
Rene Schulte <contact@rene-schulte.info>
2017-09-03 10:31:17 +0200

    Added ARCoreHorror

---
+++ ARCoreHorror/Assets/Scripts/ActiveWhenDark.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using GoogleARCore;
+using UnityEngine;
+
+public class ActiveWhenDark : MonoBehaviour
+{
+    public float IntensityThreshold = 0.15f;
+    public int MoveCloserSteps = 4;
+    public bool IsDark;
+    public GameObject[] ObjectsToSwitchOnWhenDark;
+    public Material CannonMaterialNight;
+    public Material CannonMaterialDay;
+
+    private bool _isDarkPrevious;
+    private CannonBehavior _cannonBehavior;
+
+    void Start()
+    {
+        if (ObjectsToSwitchOnWhenDark != null)
+        {
+            for (var i = 0; i < ObjectsToSwitchOnWhenDark.Length; i++)
+            {
+                ObjectsToSwitchOnWhenDark[i].SetActive(false);
+            }
+        }
+        _cannonBehavior = Camera.main.GetComponent<CannonBehavior>();
+    }
+
+    void Update()
+    {
+#if !UNITY_EDITOR
+        IsDark = Frame.LightEstimate.PixelIntensity < IntensityThreshold;
+#endif
+        var shouldMove = IsDark && _isDarkPrevious != IsDark;
+        if (ObjectsToSwitchOnWhenDark != null)
+        {
+            for (var i = 0; i < ObjectsToSwitchOnWhenDark.Length; i++)
+            {
+                ObjectsToSwitchOnWhenDark[i].SetActive(IsDark);
+                if (shouldMove)
+                {
+                    var tra = ObjectsToSwitchOnWhenDark[i].transform;
+                    var dif = Vector3.ProjectOnPlane(Camera.main.transform.position, Vector3.up) - Vector3.ProjectOnPlane(tra.position, Vector3.up);
+                    tra.position = tra.position + dif / MoveCloserSteps;
+                }
+            }
+        }
+
+        if (_cannonBehavior != null)
+        {
+            _cannonBehavior.CannonMaterial = IsDark ? CannonMaterialNight : CannonMaterialDay;
+        }
+
+        _isDarkPrevious = IsDark;
+    }
+}
---
+++ ARCoreHorror/Assets/Scripts/CannonBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+
+public class CannonBehavior : MonoBehaviour
+{
+    public float ForceMagnitude = 200f;
+    public float CannonBallSize = 0.1f;
+    public PhysicMaterial CannonBallPhysics;
+
+    public GameObject GazeCursor;
+    public Material CannonMaterial;
+    public AudioSource ShootSound;
+    public AudioClip CollisionClip;
+
+    public void Shoot()
+    {
+        //    ShootSound.Play();
+
+        var eyeball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        eyeball.transform.localScale = new Vector3(CannonBallSize, CannonBallSize, CannonBallSize);
+        eyeball.GetComponent<Renderer>().material = CannonMaterial;
+        eyeball.GetComponent<Collider>().material = CannonBallPhysics;
+
+        var rigidBody = eyeball.AddComponent<Rigidbody>();
+        rigidBody.mass = 0.5f;
+        rigidBody.angularDrag = 1.0f;
+        rigidBody.drag = 0.75f;
+        rigidBody.position = transform.position;
+        var forward = transform.forward;
+        forward = Quaternion.AngleAxis(-10, transform.right) * forward;
+        rigidBody.AddForce(forward * ForceMagnitude);
+
+        eyeball.AddComponent<AudioCollisionBehaviour>().SoundSoftCrash = CollisionClip;
+    }
+
+    void Update()
+    {
+        if (GazeCursor == null) return;
+
+        var raycastHits = Physics.RaycastAll(transform.position, transform.forward);
+        var firstHit = raycastHits.OrderBy(r => r.distance).FirstOrDefault();
+
+        GazeCursor.transform.position = firstHit.point;
+        GazeCursor.transform.forward = firstHit.normal;
+    }
+}
